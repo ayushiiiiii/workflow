f619337fbd41a129630635ae00749870d2616f7d
Priyansh Bhardwaj <priyanshbhj@gmail.com>
2019-10-26 12:11:59 +0530

    Blank

--- src/maincomponent.js
+++ src/maincomponent.js
@@ -410,9 +410,9 @@ class Main extends Component{
             routes.push(<Route exact path='/home/:projectId/complete' component={({match}) => <Complete editTasks={this.editTasks} projectId={match.params.projectId} tasks={this.state.projects.length>0?this.state.projects.filter(project => project._id==match.params.projectId)[0].tasks:[]} />} />);
         }if(this.state.user.type.admin){
             routes.push(<Route exact path='/access' component={()=> <UAccess token={this.state.token} /> }/>);
-            routes.push(<Route path='/manage/signup' component={() => <Sign addUser={this.addUser} />} />);
-            routes.push(<Route path='/Edit/:userId' component={({match}) => <Editmanage listUsers={this.listUsers} users={this.state.users} userId={match.params.userId} editUser={this.editUser} />} />);
-            routes.push(<Route path='/manage' component={() => <Manage listUsers={this.listUsers} users={this.state.users} deleteUser={this.deleteUser} />} />);
+            routes.push(<Route exact path='/manage/signup' component={() => <Sign addUser={this.addUser} />} />);
+            routes.push(<Route exact path='/Edit/:userId' component={({match}) => <Editmanage listUsers={this.listUsers} users={this.state.users} userId={match.params.userId} editUser={this.editUser} />} />);
+            routes.push(<Route exact path='/manage' component={() => <Manage listUsers={this.listUsers} users={this.state.users} deleteUser={this.deleteUser} />} />);
         }
 
         return(
@@ -422,8 +422,8 @@ class Main extends Component{
               <Route exact path='/home' component={() => <Dashboard data_entry={this.state.user.type.data_entry} logOut={this.logOut} editProject={this.editProject} getProjects={this.getProjects} token={this.state.token} projects={this.state.projects} isProjectsLoading={this.state.isProjectsLoading} />} />
               <Route exact path='/admin' component={() => <Admin admin={this.state.user.type.admin} data_entry={this.state.user.type.data_entry} logOut={this.logOut} />} />
               <Route exact path='/home/:projectId/:taskId/comments' component={({match}) => <Com user={this.state.user} comments={this.state.user.type.comments} token={this.state.token} projectId={match.params.projectId} taskId={match.params.taskId} />} />
+			  <Route exact path='/details/changepassword' component={() => <Change resetPassword={this.resetPassword} />} />
               <Route exact path='/details' component={() => <Details listUsers={this.listUsers}  users={this.state.user} />} />
-              <Route exact path='/details/changepassword' component={() => <Change resetPassword={this.resetPassword} />} />
               {routes}
               <Redirect to='/admin' />
           </Switch>
